766a95ba9994bebc2d8efa92e46d990b06576c85
Om-Borse26 <omborse1618@gmail.com>
2026-04-07 01:28:44 +0530

    importing functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,8 @@ import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
-import { serveInngest } from "inngest/express";
-import { inngest } from "./lib/inngest.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
@@ -12,9 +12,9 @@ const __dirname = path.resolve();
 
 //middlewares
 app.use(express.json());
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
-app.use("/api/inngest", serve({client: inngest, functions}));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (_req, res) => {
   res.status(200).json({ msg: "API is up and running" });
@@ -28,21 +28,20 @@ app.get("/books", (_req, res) => {
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
-const startServer = async()=>{
+const startServer = async () => {
   try {
     await connectDB();
     app.listen(ENV.PORT, () => {
       console.log("Server is running on port:", ENV.PORT);
     });
-
   } catch (error) {
-    console.error("💥 Error starting the server", error)
+    console.error("💥 Error starting the server", error);
   }
-}
+};
 
 startServer();
