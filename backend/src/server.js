bc91ba8dc67808175d2834131684d8bdbb9b8681
Om-Borse26 <omborse1618@gmail.com>
2026-04-07 21:38:01 +0530

    feat: initialize backend server with Express, Clerk authentication, and Stream chat token integration

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,9 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
+import { clerkMiddleware } from "@clerk/express";
+
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
@@ -14,15 +17,15 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
+app.use(clerkMiddleware()); //* this adds auth field to request object : req.auth()
+
 app.use("/inngest", serve({ client: inngest, functions }));
+app.get("/chat",chatRoutes);
 
 app.get("/health", (_req, res) => {
   res.status(200).json({ msg: "API is up and running" });
 });
 
-app.get("/books", (_req, res) => {
-  res.status(200).json({ msg: "Books Endpoint Hit" });
-});
 
 //make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
